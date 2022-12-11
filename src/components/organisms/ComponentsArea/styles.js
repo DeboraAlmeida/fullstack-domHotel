4911b928894b098f3e6b7014f20938bfab6f0e3d
Andrade Christian <andrade.dias.christian@gmail.com>
2022-12-11 02:55:43 -0300

    create ImageDefault

--- src/components/organisms/ComponentsArea/styles.js
+++ src/components/organisms/ComponentsArea/styles.js
@@ -3,3 +3,14 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
 
 `
+
+export const ImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  img {
+    width: 150px;
+    margin: 10px
+  }
+`
