bf557649b2b7062171e0d269f5bdda16852e5fd7
Andrade Christian <andrade.dias.christian@gmail.com>
2022-12-15 21:58:06 -0300

    create default template

--- src/components/atoms/NavBar/styles.js
+++ src/components/atoms/NavBar/styles.js
@@ -6,4 +6,14 @@ export const Nav = styled.nav`
   width: fit-content;
   padding: 5px 10px;
   margin: 0 auto;
+
+  li {
+    margin: 0px 10px;
+    font-weight: bold;
+    
+    a {
+      color: pink;
+      text-decoration: none;
+    }
+  }
 `
