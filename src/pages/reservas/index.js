067e9a49628e79ebfe7085dd4d2c08a357bfa4e5
Debora Almeida <deboraalmeida89@gmail.com>
2022-12-23 23:31:46 -0300

    Added Step 1 in Reserve Page - inputs

--- src/pages/reservas/index.js
+++ src/pages/reservas/index.js
@@ -1,11 +1,69 @@
 // Arquivo criado: 15/12/2022 às 20:49
-import React from 'react'
+import React, { useState } from 'react'
+import GenericInput from '../../components/atoms/GenericInput'
+import GenericLabel from '../../components/atoms/GenericLabel'
+import MiniTitle from '../../components/atoms/MiniTitle'
+import PrincipalTitle from '../../components/atoms/PrincipalTitle'
 import * as S from './styles'
 
 export const Reservas = () => {
+  const [inputsCollection] = useState([
+    {
+      type: 'text',
+      label: 'Nome'
+    },
+    {
+      type: 'email',
+      label: 'E-mail'
+    },
+    {
+      type: 'number',
+      label: 'Telefone'
+    }
+
+  ])
+
+  const [inputsReserve] = useState([
+    {
+      type: 'date',
+      label: 'Data de checkin'
+    },
+    {
+      type: 'date',
+      label: 'Data de checkout'
+    },
+    {
+      type: 'number',
+      label: 'Número de adultos'
+    },
+    {
+      type: 'number',
+      label: 'Número de crianças'
+    }
+    
+  ])
 
   return (
-    <S.p>Reservas</S.p>
+    <>
+      <PrincipalTitle>Reserve sua Acomodação</PrincipalTitle>
+      <MiniTitle span='Passo 1: ' text='Insira seus dados' />
+      <S.FormContainer>
+        {inputsCollection.map((element, index) => (
+          <S.Container key={index}>
+            <GenericLabel for={element.id}>{element.label}</GenericLabel>
+            <GenericInput type={element.type} id={element.id} />
+          </S.Container>
+        ))}  
+      </S.FormContainer>
+      <S.ContainerReserve>
+        {inputsReserve.map((element, index) => (
+          <S.ReserveItem key={index}>
+            <GenericLabel for={element.id}>{element.label}</GenericLabel>
+            <GenericInput type={element.type} id={element.id} />
+          </S.ReserveItem>
+        ))}   
+      </S.ContainerReserve>
+    </>
   )
 
 }
--- src/pages/reservas/styles.js
+++ src/pages/reservas/styles.js
@@ -1,5 +1,37 @@
 import styled from 'styled-components'
 
-export const p = styled.p`
-  color: black;
+export const FormContainer = styled.form`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  @media screen and (maxwidth: 760px) {
+    margin-top: 25px;
+  }
+  `
+export const Container = styled.div`
+  max-width: 600px;
+  width: 100%;
+  
+`
+export const ContainerReserve = styled.div`
+  display: flex;
+  justify-content:space-evenly;
+  @media screen and (max-width: 870px){
+    flex-wrap: wrap;
+  }
+  @media screen and (max-width: 667px){
+    flex-direction:column;
+    align-items: center;
+  }
+`
+export const ReserveItem = styled.div`
+  width: 20%;
+  @media screen and (max-width: 870px) {
+    width: 40%;
+  }
+  @media screen and (max-width: 667px) {
+    width:100%;
+  }
 `
