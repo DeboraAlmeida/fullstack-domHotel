e8b96ffffa6578ff44d0ef66d259877346eca38c
Debora Almeida <deboraalmeida89@gmail.com>
2022-12-26 22:02:28 -0300

    Added resume box

--- src/pages/reservas/index.js
+++ src/pages/reservas/index.js
@@ -1,5 +1,6 @@
 // Arquivo criado: 15/12/2022 às 20:49
 import React, { useState } from 'react'
+import Button from '../../components/atoms/Button'
 import GenericInput from '../../components/atoms/GenericInput'
 import GenericLabel from '../../components/atoms/GenericLabel'
 import MiniTitle from '../../components/atoms/MiniTitle'
@@ -28,17 +29,17 @@ export const Reservas = () => {
 
   const [inputsReserve] = useState([
     {
-      id: '1',
+      id: 'checkin',
       type: 'date',
       label: 'Data de checkin'
     },
     {
-      id: '2',
+      id: 'checkout',
       type: 'date',
       label: 'Data de checkout'
     },
     {
-      id: '3',
+      id: 'adultos',
       type: 'number',
       label: 'Número de adultos',
       placeholder: '1',
@@ -46,7 +47,7 @@ export const Reservas = () => {
       min: '1'
     },
     {
-      id: '4',
+      id: 'criancas',
       type: 'number',
       label: 'Número de crianças',
       max: '4',
@@ -54,27 +55,69 @@ export const Reservas = () => {
     }
     
   ])
+  
+  const [ResumeItens] = useState([
+    { 
+      id: 'resume-room',
+      name: 'Quarto: ',
+      content: 'xxx',
+      class: 'marginBottom'
+    },
+    { 
+      id: 'resume-checkin', 
+      name: 'Checkin: ', 
+      content: 'xxx', 
+      class: ''
+    },
+    { 
+      id: 'resume-checkout',
+      name: 'Checkout: ',
+      content: 'xxx',
+      class: 'marginBottom'
+    },
+    { 
+      id: 'resume-people',
+      name: 'Pessoas: ',
+      content: 'xxx',
+      class: ''
+    },
+    { 
+      id: 'resume-services',
+      name: 'Serviços Adicionais: ',
+      content: 'xxx',
+      class: 'marginBottom'
+    }])
 
   return (
     <S.PrincipalContainer>
       <PrincipalTitle>Reserve sua Acomodação</PrincipalTitle>
       <MiniTitle span='Passo 1: ' text='Insira seus dados' />
       <S.FormContainer>
+        <S.DataContainer>
         {inputsCollection.map((element, index) => (
           <S.Container key={index}>
             <GenericLabel for={element.id}>{element.label}</GenericLabel>
             <GenericInput type={element.type} id={element.id}/>
           </S.Container>
-        ))}  
+        ))} 
+        </S.DataContainer> 
+        <S.ContainerReserve>
+          {inputsReserve.map((element, index) => (
+            <S.ReserveItem key={index}>
+              <GenericLabel for={element.id}>{element.label}</GenericLabel>
+              <GenericInput type={element.type} id={element.id} placeholder={element.placeholder} min={element.min} max={element.max} name={element.id} />
+            </S.ReserveItem>
+          ))}   
+        </S.ContainerReserve>
       </S.FormContainer>
-      <S.ContainerReserve>
-        {inputsReserve.map((element, index) => (
-          <S.ReserveItem key={index}>
-            <GenericLabel for={element.id}>{element.label}</GenericLabel>
-            <GenericInput type={element.type} id={element.id} placeholder={element.placeholder} min={element.min} max={element.max} />
-          </S.ReserveItem>
-        ))}   
-      </S.ContainerReserve>
+        <S.ContainerResume>
+            <ul>
+            {ResumeItens.map((element, index) => (
+              <li key ={index} className={element.class}>{element.name} {element.content} </li>
+            ))}
+            </ul>
+            <Button width='100%'>Confirmar</Button>
+        </S.ContainerResume>
     </S.PrincipalContainer>
   )
 
