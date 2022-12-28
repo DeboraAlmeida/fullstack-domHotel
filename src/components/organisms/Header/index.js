bd63d20703c4037cc2330bb1bd2421e8360bc3b5
Andrade Christian <andrade.dias.christian@gmail.com>
2022-12-28 18:22:45 -0300

    adjust header with login

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -1,14 +1,23 @@
 // Arquivo criado: 20/12/2022 às 14:27
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../../../assets/dom_logo.png'
 import Anchor from '../../atoms/Anchor'
 import Button from '../../atoms/Button'
 import ImageDefault from '../../atoms/ImageDefault'
+import Modal from '../../atoms/Modal'
 import Navbar from '../../atoms/NavBar'
+import LoginContent from '../LoginContent'
 import * as S from './styles'
 
 export const Header = () => {
+  const [showModal, setShowModal] = useState(false)
+  const [isLogged, setIsLogged] = useState(false)
 
+  useEffect(() => {
+    if (isLogged) {
+      setShowModal(false)
+    }
+  }, [isLogged])
   return (
    <S.Header>
     <div className='-body'>
@@ -19,13 +28,19 @@ export const Header = () => {
         <h3>Para viver momentos inesquecíves</h3>
       </div>
       <div className='-body-right'>
-        <Anchor href='/' msg='Olá Usuário' />
-        <Anchor href='/' msg='Sair' />
-        <Button 
-          paddingHorizontal="6px"
-          paddingVertical="6px"
-          className="-body-right-button"
-        >CADASTRE-SE</Button>
+        {isLogged && (
+          <>
+            <Anchor href='#' msg={`Olá ${'Usuário'}`} />
+            <Anchor action={() => setIsLogged(false)} msg='Sair' />
+          </>
+        )}
+        {!isLogged && (
+          <Button action={() => setShowModal(true)} 
+            paddingHorizontal="10px"
+            paddingVertical="10px"
+            className="-body-right-button"
+          >ENTRAR</Button>
+        )}
       </div>
     </div>
       <div className='-navbar'>
@@ -33,6 +48,9 @@ export const Header = () => {
           <Navbar />
         </div>
       </div>
+      <Modal isOpen={showModal} setIsOpen={setShowModal}>
+        <LoginContent type={'sign-in'} setIsLogged={setIsLogged}/>
+      </Modal>
    </S.Header>
   )
 
