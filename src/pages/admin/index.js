c230b79c33cfe1c04e2fcf3875231a6e9c9421a0
Andrade Christian <andrade.dias.christian@gmail.com>
2023-01-18 17:59:32 -0300

    set page admin

---
+++ src/pages/admin/index.js
@@ -0,0 +1,15 @@
+// Arquivo criado: 16/02/2023
+import React from 'react'
+import LoginAdmin from '../../components/organisms/LoginAdmin'
+import * as S from './styles'
+
+
+export const Admin = () => {
+  return (
+    <S.Wrapper>
+      <LoginAdmin />
+    </S.Wrapper>
+  )
+}
+
+export default Admin
