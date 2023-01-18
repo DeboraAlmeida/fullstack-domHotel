45352e830abc0385726415f4899ab915b0d92933
Andrade Christian <andrade.dias.christian@gmail.com>
2023-01-18 17:59:20 -0300

    create login admin component

---
+++ src/components/organisms/LoginAdmin/index.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from 'react'
+import { validateEmail } from '../../../utils/validateFields.js'
+import Button from '../../atoms/Button/index.js'
+import GenericInput from '../../atoms/GenericInput/index.js'
+import GenericLabel from '../../atoms/GenericLabel/index.js'
+import MiniTitle from '../../atoms/MiniTitle/index.js'
+import { adminLogins } from './adminLogins.js'
+import * as S from './styles.js'
+
+const LoginAdmin = () => {
+  const [isLogged, setIsLogged] = useState(false)
+
+
+  const [valueField, setValueFields] = useState(
+    {
+      email: '',
+      password: ''
+    }
+  )
+  const [errorFields, setErrosFields] = useState(
+    {
+      email: false,
+      password: false
+    }
+  )
+
+  const handleEmail = (value) => {
+    setValueFields((prev) => ({ ...prev, email: value }))
+    if (validateEmail(valueField.email)) {
+      setErrosFields((prev) => ({ ...prev, email: true }))
+      return
+    }
+    setErrosFields((prev) => ({ ...prev, email: false }))
+  }
+
+  const handlePassword = (value) => {
+    setValueFields((prev) => ({ ...prev, password: value }))
+    setErrosFields((prev) => ({ ...prev, password: false }))
+  }
+
+  const actionSignIn = (data, logins) => {
+    if (data.email === '' || data.password === '') {
+      if (data.email === '') {
+        setErrosFields((prev) => ({ ...prev, email: true }))
+      }
+      if (data.password === '') {
+        setErrosFields((prev) => ({ ...prev, password: true }))
+      }
+      return
+    }
+    let result = false
+    logins.forEach(user => {
+      if (user.email === data.email && user.password === data.password) {
+        result = true
+      }
+    })
+    if (result) {
+      setIsLogged(true)
+      sessionStorage.setItem('isLoggedAdmin', JSON.stringify({ ...data, isLogged: true }))
+    } else {
+      alert('Administrador não cadastrado!')
+      setIsLogged(false)
+    }    
+  }
+
+  const handleButton = () => {
+    const data = {
+      email: valueField.email,
+      password: valueField.password
+    }
+    actionSignIn(data, adminLogins)
+  }
+
+  const inputsSignIn = [
+    { 
+      id: 'email-login',
+      label: 'E-mail:',
+      method: handleEmail,
+      model: errorFields.email,
+      valueId: 'email',
+      type: 'email'
+    },
+    { 
+      id: 'password-login',
+      label: 'Senha:',
+      method: handlePassword,
+      model: errorFields.password,
+      valueId: 'password',
+      type: 'password'
+    }
+  ]
+
+  useEffect(() => {
+    if (JSON.parse(sessionStorage.getItem('isLoggedAdmin'))) {
+      const result = JSON.parse(sessionStorage.getItem('isLoggedAdmin'))
+      setIsLogged(result.isLogged)
+      return
+    }
+    setIsLogged(false)
+  }, [])
+
+  return (
+    <S.Wrapper>
+      {!isLogged && (
+        <S.WrapperLogin>
+          <MiniTitle text={'Login Admin'}/>
+          {inputsSignIn.map((element, index) => (
+            <S.ContainerInputSignIn key={index}>
+              <GenericLabel for={element.id}>{element.label}</GenericLabel>
+              <GenericInput type={element.type} id={element.id} onChange={(e) => element.method(e.target.value)} error={element.model} value={valueField[`${element.valueId}`]}/>
+            </ S.ContainerInputSignIn>
+          ))}
+          <Button disabled={(errorFields.email || errorFields.password)} action={handleButton}>Entrar</Button>
+          </S.WrapperLogin>
+      )}
+      {isLogged && (
+        <>Administrador logado!</>
+      )}      
+    </S.Wrapper>
+  )
+}
+
+export default LoginAdmin
---
+++ src/components/organisms/LoginAdmin/styles.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const WrapperLogin = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  max-width: 768px;
+  margin: auto;
+  padding: 100px 60px;
+  box-shadow: 10px 10px 5px #a0a0a0ea;
+  border-radius: 10px;
+  h2{
+    font-size: 32px;
+    text-transform: uppercase;
+    margin-bottom: 30px;
+  }
+  button {
+    margin-top: 50px;
+    width: 250px;
+  }
+  @media screen and (max-width: 768px) {
+    padding: 0;
+    box-shadow: none;
+    border-radius: none;
+    border: none;
+  }
+`
+
+export const ContainerInputSignIn = styled.div`
+  width: 100%; 
+  input[type = 'password']{
+    margin-bottom: 0;
+  }
+`
