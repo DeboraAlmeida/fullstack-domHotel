7fa9a3a11183bc84a9df614e881f66e28fb73922
Andrade Christian <andrade.dias.christian@gmail.com>
2023-02-23 20:58:51 -0300

    page consumo admin

--- client/src/components/organisms/ControllerAdmin/index.js
+++ client/src/components/organisms/ControllerAdmin/index.js
@@ -1,9 +1,9 @@
 // Arquivo criado: 19/01/2023 às 15:43
 import React, { useEffect, useState } from 'react'
+import ConsumoAdmin from '../ConsumoAdmin'
 import FuncionariosAdmin from '../FuncionariosAdmin'
 import HeaderAdmin from '../HeaderAdmin'
 import HomeAdmin from '../HomeAdmin'
-import HospedesAdmin from '../HospedesAdmin'
 import ReservasAdmin from '../ReservasAdmin'
 
 
@@ -19,7 +19,7 @@ const ControllerAdmin = () => {
         case 'home':
           return setComponent(<HomeAdmin />)
         case 'hospedes':
-          return setComponent(<HospedesAdmin />)
+          return setComponent(<ConsumoAdmin />)
         case 'funcionarios':
           return setComponent(<FuncionariosAdmin />)
         case 'reservas':
