06804568061d78348a5dafffde7d22f7eaa8b14e
Andrade Christian <andrade.dias.christian@gmail.com>
2023-02-23 20:56:39 -0300

    component consumo admin

---
+++ client/src/components/organisms/ConsumoAdmin/styles.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  /* margin: 0 auto;
+  margin-top: 20px;
+  width: 100%;
+  max-width: 600px; */
+`
+export const Title = styled.h3`
+  /* font-weight: normal; */
+`
+
+export const Form = styled.form`
+  margin-top: 50px;
+  display: flex; 
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 400px;
+  }
+`
+
+export const Button = styled.div`
+  button{
+    width: 100%;
+    max-width: 200px;
+    margin: auto;
+  }
+`
+export const ContainerReserves = styled.div`
+`
+
+export const ContainerProducts = styled.div`
+
+`
