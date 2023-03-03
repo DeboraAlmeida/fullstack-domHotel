4d4412ab9184bdd4fdd34402b0e4943b481aea27
Larissa Cordovil <larissacordovil18@gmail.com>
2023-03-02 21:07:03 -0300

    funcionarios

--- server/src/routes/funcionarios.js
+++ server/src/routes/funcionarios.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import FuncionariosController from '../controller/funcionariosController.js'
+import decrypt from '../middlewares/decrypt.js'
 
 const router = express.Router()
 
 router
   .get('/funcionarios', FuncionariosController.getWorkers)
-  .get('/funcionarios/Ativos', FuncionariosController.getWorkersActived)
+  .get('/funcionariosAtivos', FuncionariosController.getActiveWorkers)
+  .post('/funcionariosCadastro', decrypt, FuncionariosController.postNewWorkers)
+  .put('/funcionariosAtualiza', FuncionariosController.updateWorkers)
 
 export default router
