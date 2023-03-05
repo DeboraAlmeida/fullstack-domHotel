c593197ed8010ad28d6c7ec411810239ab66c492
Flaviano Rodrigues <flavianodev@gmail.com>
2023-03-05 16:56:09 -0300

    Ajustando os servioços da area administrativa

--- client/src/services/postExpenditure.tsx
+++ client/src/services/postExpenditure.tsx
@@ -1,3 +1,5 @@
+import getIdToken from "utils/getIdToken"
+
 const postExpenditure = async (payload: any) => {
     const {reserve, product} = payload
     const obj = {
@@ -11,12 +13,12 @@ const postExpenditure = async (payload: any) => {
                 'Content-Type': 'application/json',
                 'Connection': 'keep-alive',
                 'Keep-Alive': 'timeout=5',
+                'Authorization': `Bearer ${getIdToken('admin')}`
             }, body: JSON.stringify(obj) 
         })
         const result = await response.json()
         return result.data
     } catch (error) {
-        console.error(`post reserves failed ${error}`)
         return 'Erro ao cadastrar o produto!'
     }
 }
