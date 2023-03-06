432c67089248cfdcb5ea7d2398f0acde1b4dbd67
Flaviano Rodrigues <flavianodev@gmail.com>
2023-03-06 11:48:39 -0300

    melhoria no getMouthReserves

--- client/src/services/getMouthReserves.tsx
+++ client/src/services/getMouthReserves.tsx
@@ -1,28 +1,34 @@
 import getIdToken from "utils/getIdToken";
 
 const getMouthReserves = async () => {
-  const date = new Date();
-  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-  let lastDayDate = lastDay.toLocaleDateString()
-  const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-  let firstDayDate = firstDay.toLocaleDateString()
-  lastDayDate = lastDayDate.split('/').reverse().join('-')
-  firstDayDate = firstDayDate.split('/').reverse().join('-')
+
+  const getFullDate = (type: 'first' | 'last') => {
+    const date = new Date();
+
+    const data = type === 'first'
+      ? new Date(date.getFullYear(), date.getMonth(), 1) 
+      : new Date(date.getFullYear(), date.getMonth() + 1, 0)
+
+   
+    return data.toLocaleDateString().split('/').reverse().join('-')
+  }
+
+
   try {
-    const response = await fetch(`http://localhost:3002/reservas-pela-data/${firstDayDate}/${lastDayDate}`, {
-        method: 'GET', headers: {
-            'X-Powered-By': 'Express',
-            'Content-Type': 'application/json',
-            'Connection': 'keep-alive',
-            'Keep-Alive': 'timeout=5',
-            'Authorization': `Bearer ${getIdToken('admin')}`
-        }
+    const response = await fetch(`http://localhost:3002/reservas-pela-data/${getFullDate('first')}/${getFullDate('last')}`, {
+      method: 'GET', headers: {
+        'X-Powered-By': 'Express',
+        'Content-Type': 'application/json',
+        'Connection': 'keep-alive',
+        'Keep-Alive': 'timeout=5',
+        'Authorization': `Bearer ${getIdToken('admin')}`
+      }
     })
     const result = await response.json()
     return result.data.length
   } catch (error) {
     return
-}
+  }
 }
 
 export default getMouthReserves
