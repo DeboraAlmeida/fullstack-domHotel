caea23813a10ab7acacbca48542ff388cac604a3
Flaviano Rodrigues <flavianodev@gmail.com>
2023-03-07 10:43:10 -0300

    resolvendo erro contatoController

--- server/src/controller/contatoController.js
+++ server/src/controller/contatoController.js
@@ -1,31 +1,25 @@
 import sqlDB from '../app/mysql.js'
 
-class ContatoController {
+export default class ContatoController {
 
   static postContato = (req, res) => {
     const payload = req.body 
 
-    const obj = {
-      name: payload.name,
-      email: payload.email,
-      comment: payload.comment,
-      subject: payload.subject
-    }
-
-    sqlDB.query('INSERT INTO `contact` (name, email, comment, subject) VALUES (?, ?, ?, ?)', [obj.name, obj.email, obj.comment, obj.subject], (err, data) => {
+    sqlDB.query('INSERT INTO `contact` (name, email, comment, subject, createdAt) VALUES (?, ?, ?, ?, ?)', [payload.name, payload.email, payload.comment, payload.subject, new Date()], (err, data) => {
 
       if (err) {
-        res.status(500).send({ message: err.message })
+        res.status(500).send({ 
+          status: 500,
+          message: err.message
+        })
         return
       }
 
       res.status(201).json({
         status: 201,
-        data: `Mensagem de contato cadastrada com sucesso. ${data}`
+        data: 'Mensagem de contato cadastrada com sucesso!'
       })
     })
   }
 
 }
-
-export default ContatoController
