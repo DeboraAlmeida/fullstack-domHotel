ae9a4f50a500becdd1052305b5b0c581ebd3f3dd
Andrade Christian <andrade.dias.christian@gmail.com>
2023-03-06 21:11:29 -0300

    create controller and router employee

--- server/src/routes/funcionarios.js
+++ server/src/routes/funcionarios.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import FuncionariosController from '../controller/funcionariosController.js'
-import decrypt from '../middlewares/decrypt.js'
 
 const router = express.Router()
 
 router
   .get('/funcionarios', FuncionariosController.getWorkers)
+  .get('/cargos', FuncionariosController.getOffices)
   .get('/funcionariosAtivos', FuncionariosController.getActiveWorkers)
-  .post('/funcionariosCadastro', decrypt, FuncionariosController.postNewWorkers)
+  .get('/funcionariosInativos', FuncionariosController.getInactiveWorkers)
+  .post('/funcionariosCadastro', FuncionariosController.postNewWorkers)
   .put('/funcionariosAtualiza', FuncionariosController.updateWorkers)
   // .get('/funcionarios/Ativos', FuncionariosController.getWorkersActived)
   .get('/funcionarios/AtivosTotal', FuncionariosController.getWorkersNumber)
